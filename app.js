4d13105224a5a464dbe6c90fcc8ae93ca42355ae
Fully34 <cfullinw34@gmail.com>
2015-07-21 14:21:12 -0600

    finished

--- app.js
+++ app.js
@@ -9,10 +9,15 @@ var indexController = require('./controllers/index.js');
 var app = express();
 app.set('view engine', 'jade');
 app.set('views', __dirname + '/views');
-app.use(express.static(__dirname + '/public'));
+
+
+// app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({extended: false}));
 
 app.get('/', indexController.index);
+
+app.get('/:filename', indexController.staticServer);
+
 
 var server = app.listen(7347, function() {
 	console.log('Express server listening on port ' + server.address().port);
--- controllers/index.js
+++ controllers/index.js
@@ -1,20 +1,46 @@
 //============================== FOR NODE2 PART II ==============================//
 
+var fs = require('fs');
 
 var indexController = {
+
     index: function(req, res) {
 
-        var fs = require('fs');
+        res.render('index');
+      //   fs.readFile('data.txt', function(err, data) {
 
-        fs.readFile('data.txt', function(err, data) {
+      //       res.header('Content-Type', 'text/html');
+    		// res.send(data);
+ 
+      //   });  
+	},
+    
+    // static file server bonus 
+    staticServer : function(req, res) {
 
-            res.header('Content-Type', 'text/html')
-    		res.send(data);
+        // this just looks at the url and checks the pram
+        var name = req.params.filename;
+        // console.log(name)
 
-            
-        })    
+        //need to specify route
+            // since the function is getting called in app.js, we don't need to go down a level in the directory tree (ie: it is getting called from the mother directory)
+            // the second argument is an optional encoding argument
+                // makes it readable in the console
+        fs.readFile('public/' + name, 'utf-8', function(err, data) {
 
-	}
+            // throw an error if we encounter one
+            if (err) {
+
+                throw err;
+
+            } else {
+
+                // console.log(data);
+                res.header('Content-Type', 'text/html');
+                res.send(data);
+            }
+        });
+    }
 };
 
 module.exports = indexController;
